0d8e9235b33d9cc218b70c902ffa44d93117824d
hana-kr <hanakariman@yahoo.com>
2021-11-18 19:48:58 +0330

    finished

---
+++ Bird Game/Assets/Scripts/Player.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 initialPoint;
+    bool surface = false;
+    public int BirdCount = 0 ;
+    public int CoinCount = 0;
+    public EventSystemCustom eventSystem;
+    public Animator anim;
+    public GameObject Chest;
+    public GameObject Coin;
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+        initialPoint = transform.position;
+    }
+    private void Update()
+    {
+        if(surface == true)
+        {
+            StartCoroutine(ExecuteAfterTime(3));
+            surface = false;
+            Debug.Log( "birds:" + BirdCount);
+        }
+        if(Coin != null)
+        {
+            CoinCount += 20;
+            Debug.Log( "coins:" +CoinCount);
+            Destroy(Coin);
+            Debug.Log("hereeee");
+            eventSystem.UpdatedCoins.Invoke();
+        }
+        if(BirdCount >= 30)
+        {
+            eventSystem.GameOver.Invoke();
+        }
+        if(Chest != null && CoinCount>= 20)
+        { 
+            eventSystem.Win.Invoke();
+            Debug.Log("you wonn");
+        }
+        
+    }
+    
+    public void OnMouseUp()
+    {
+        Vector2 direction = initialPoint - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(direction *350);
+        GetComponent<Rigidbody2D>().gravityScale = 0.4f;
+        BirdCount++;
+        eventSystem.UpdatedBirds.Invoke();
+    }
+    public void OnMouseDrag()
+    {
+        Vector3 PlayerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(PlayerPosition.x , PlayerPosition.y);
+        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeathZone")) 
+        {
+         
+            StartCoroutine(ExecuteAfterTime(3));
+
+        }
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+        
+            Coin = collision.gameObject;
+
+        }
+        if (collision.gameObject.CompareTag("Chest"))
+        {
+            Chest = collision.gameObject;
+        }
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.SetBool("Hit", true);
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            surface = true;
+           
+        }
+        
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        anim.SetBool("Hit", false);
+      
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            surface = false;
+            Debug.Log(surface);
+
+        }
+
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Vector2 StopForce = new Vector2(0,0);
+
+        transform.position = initialPoint;
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        transform.position = initialPoint;
+        
+
+    }
+}
---
+++ Bird Game/Assets/Scripts/UiManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public Text CoinCount;
+    public Text BirdCount;
+    public EventSystemCustom eventSystem;
+    public Player player;
+    public Text gameOverTex;
+    public Text WonText;
+    public Button Restartbutton;
+
+    void Start()
+    {
+        Restartbutton.gameObject.SetActive(false);
+        WonText.gameObject.SetActive(false);
+        gameOverTex.gameObject.SetActive(false);
+        eventSystem.UpdatedCoins.AddListener(UpdateCoins);
+        eventSystem.UpdatedBirds.AddListener(UpdateBirds);
+        eventSystem.GameOver.AddListener(GameOverText);
+        eventSystem.Win.AddListener(WinText);
+    }
+    public void WinText()
+    {
+        WonText.gameObject.SetActive(true);
+    }
+
+    public void GameOverText()
+    {
+        gameOverTex.gameObject.SetActive(true);
+        Restartbutton.gameObject.SetActive(true);
+    }
+    public void UpdateCoins()
+    {
+        Debug.Log("UPDATE COINS");
+        CoinCount.text = player.CoinCount.ToString() + " Coins Collected";
+
+    }
+    public void UpdateBirds()
+    {
+        Debug.Log("UPDATE BIRDS");
+        BirdCount.text = player.BirdCount.ToString() + " Launched Birds";
+
+
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
